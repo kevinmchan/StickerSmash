78e9aa18625100f7969586f1960662ead175243f
Kevin Chan <kevinmchan@live.com>
2024-04-12 14:32:17 -0400

    Update spacing

--- components/Button.js
+++ components/Button.js
@@ -2,56 +2,56 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Button({ label, theme, onPress }) {
-    if (theme === 'primary') {
-        return <PrimaryButton label={label} onPress={onPress} />;
-    }
+  if (theme === 'primary') {
+    return <PrimaryButton label={label} onPress={onPress} />;
+  }
 
-    return (
-        <View style={styles.buttonContainer}>
-            <Pressable onPress={onPress} style={styles.button}>
-                <Text style={styles.buttonLabel}>{label}</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable onPress={onPress} style={styles.button}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 function PrimaryButton({ label, onPress }) {
-    return (
-        <View style={[styles.buttonContainer, { borderColor: '#ffd33d' }]}>
-            <Pressable
-                onPress={onPress}
-                style={[styles.button, { backgroundColor: '#fff' }]}>
-                <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
-                <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={[styles.buttonContainer, { borderColor: '#ffd33d' }]}>
+      <Pressable
+        onPress={onPress}
+        style={[styles.button, { backgroundColor: '#fff' }]}>
+        <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        padding: 3,
-        margin: 3,
-        borderWidth: 3,
-        borderRadius: 18,
-        borderColor: '#fff',
-    },
-    button: {
-        borderRadius: 12,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-    },
-    buttonLabel: {
-        color: '#fff',
-        fontSize: 16,
-    },
-    buttonIcon: {
-        paddingRight: 8,
-    },
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    padding: 3,
+    margin: 3,
+    borderWidth: 3,
+    borderRadius: 18,
+    borderColor: '#fff',
+  },
+  button: {
+    borderRadius: 12,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
 });
--- components/CircleButton.js
+++ components/CircleButton.js
@@ -2,30 +2,30 @@ import { StyleSheet, Pressable, View } from "react-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function CircleButton({ onPress }) {
-    return (
-        <View style={styles.buttonContainer}>
-            <Pressable onPress={onPress} style={styles.button}>
-                <MaterialIcons name="add" size={32} color="#25292e" />
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable onPress={onPress} style={styles.button}>
+        <MaterialIcons name="add" size={32} color="#25292e" />
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 68,
-        height: 68,
-        borderRadius: 34,
-        padding: 3,
-        borderColor: '#ffd33d',
-        borderWidth: 4,
-    },
-    button: {
-        borderRadius: 34,
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  buttonContainer: {
+    width: 68,
+    height: 68,
+    borderRadius: 34,
+    padding: 3,
+    borderColor: '#ffd33d',
+    borderWidth: 4,
+  },
+  button: {
+    borderRadius: 34,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
